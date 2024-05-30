ed399d57c1c8482b20686d9b5448b21c84a97c0a
Michaël Bontyes <michaelbontyes@gmail.com>
2024-05-30 16:28:46 -0400

    O3 form schema and script updated

--- xlsx-to-O3-forms.py
+++ xlsx-to-O3-forms.py
@@ -1,28 +1,46 @@
 import pandas as pd
 import json
 import uuid
+import re
 
-def xls_to_json(xls_file_path, json_file_path):
+def xls_to_json(xls_file_path, json_file_path, form_schema):
     # Conversion table for rendering options
     rendering_conversion = {
         'Coded': 'radio',
         'Text': 'text',
         'Numeric': 'number',
-        'Boolean': 'boolean'
+        'Boolean': 'radio',
+        'Select': 'select',
+        'MultiSelect': 'multiCheckbox',
+        'Date': 'date'
     }
 
-    # Read the XLS data from the specified sheet
-    df = pd.read_excel(xls_file_path, engine='openpyxl', sheet_name='F01-MHPSS_Baseline')
-    
+    # Read the XLS data from the specified sheet with the correct header
+    df = pd.read_excel(xls_file_path, engine='openpyxl', sheet_name='F01-MHPSS_Baseline', header=1)
+    optionsets_df = pd.read_excel(xls_file_path, engine='openpyxl', sheet_name='OptionSets', header=1)
+
+    # Create a dictionary for OptionSets
+    optionsets_dict = {}
+    for _, row in optionsets_df.iterrows():
+        option_set_name = row['OptionSet name']
+        option = row['Answers']
+        if isinstance(option, str):
+            option = re.sub(r'^\d+(\.\d+)?\s*|\.\s*', '', option)  # Remove leading numerotations and ". "
+        if option_set_name not in optionsets_dict:
+            optionsets_dict[option_set_name] = []
+        optionsets_dict[option_set_name].append(option)
+
     # Transform XLS data into JSON structure
+    form_name = 'F01-MHPSS_Baseline'
     json_data = {
-        'name': 'Page name',
+        'name': form_name,
         'pages': [],
-        'processor': 'EncounterFormProcessor',
-        'encounterType': 'dd528487-82a5-4082-9c72-ed246bd49591', # UUID of a Consultation encounter
-        'referencedForms': [],
+        'processor': form_schema['processor'],
+        'encounterType': form_schema['encounterType'],
+        'referencedForms': form_schema['referencedForms'],
         'uuid': str(uuid.uuid4()),  # Generate a new UUID
-        'version': '1.0'
+        'version': form_schema['version'],
+        'description': form_name
     }
     current_page_label = None
     current_section_label = None
@@ -30,17 +48,18 @@ def xls_to_json(xls_file_path, json_file_path):
 
     # Process each row in the DataFrame
     for index, row in df.iterrows():
-        page_label = row['Page'] # Use the page value from the XLSX file as the description
-        json_data['description'] = page_label  
-        json_data['name'] = page_label  
+        # Skip empty or null questions
+        if pd.isna(row['Question']):
+            continue
+        
+        page_label = row['Page']  # Use the page value from the XLSX file as the description
         section_label = row['Section']
-        question_label = row['Question']
-        required = row.get('Mandatory', False)  # Make "Required" optional
+        question_label = row['Label if different'] if pd.notna(row['Label if different']) else row['Question']
+        required = row.get('Mandatory', False) == True  # Make "Mandatory" optional
         question_id = row['Question ID']
         rendering = rendering_conversion.get(row['Datatype'], '')  # Use conversion table
         concept = row.get('Question', '')
-        answer_concept = row.get('Answer', '')
-        answer_label = row.get('Answer', '')
+        option_set_name = row.get('OptionSet name')
 
         # Check if we need to add a new page
         if page_label != current_page_label:
@@ -56,52 +75,55 @@ def xls_to_json(xls_file_path, json_file_path):
             current_section_label = section_label
             json_data['pages'][-1]['sections'].append({
                 'label': section_label,
-                'isExpanded': 'true',
+                'isExpanded': False,
                 'questions': []
             })
 
-        # Check if the datatype is "boolean" and set the rendering and answers accordingly
-        if rendering == 'boolean':
-            rendering = 'radio'
-            question_options = {
-                'rendering': rendering,
-                'answers': [
-                    {'concept': 'Yes', 'label': 'Yes'},
-                    {'concept': 'No', 'label': 'No'}
-                ]
-            }
-        else:
-            rendering = rendering_conversion.get(row['Datatype'], '')  # Use conversion table
-            question_options = {'rendering': rendering, 'answers': []}
+        # Set the rendering and answers accordingly
+        question_options = {
+            'rendering': rendering,
+            'concept': concept,
+            'conceptMappings': [],
+            'answers': []
+        }
+
+        # Add answers from OptionSets if the question type is radio, select, or multiSelect and an OptionSet name is defined
+        if rendering in ['radio', 'select', 'multiCheckbox'] and option_set_name in optionsets_dict:
+            for option in optionsets_dict[option_set_name]:
+                question_options['answers'].append({
+                    'concept': option,
+                    'label': option
+                })
+        elif rendering == 'boolean':
+            question_options['answers'] = [
+                {'concept': 'Yes', 'label': 'Yes'},
+                {'concept': 'No', 'label': 'No'}
+            ]
 
         # Create or update the question
+        question = {
+            'label': question_label,
+            'type': 'obs',
+            'required': required,
+            'id': question_id,
+            'questionOptions': question_options,
+            'validators': []
+        }
+
         if question_id not in questions_dict:
-            question = {
-                'label': question_label,
-                'id': question_id,
-                'questionOptions': question_options
-            }
-            if pd.notna(concept) and concept != '':
-                question['questionOptions']['concept'] = concept
-            if 'Required' in df.columns and pd.notna(row['Required']):
-                question['required'] = row['Required']
             questions_dict[question_id] = question
             json_data['pages'][-1]['sections'][-1]['questions'].append(question)
-        else:
-            question = questions_dict[question_id]
-
-        # Add answers if the question type is radio or coded
-        if rendering in ['radio', 'coded'] and pd.notna(answer_concept) and answer_concept != '' and pd.notna(answer_label) and answer_label != '':
-            question['questionOptions']['answers'].append({
-                'concept': answer_concept,
-                'label': answer_label
-            })
 
     # Save the JSON data to a file
     with open(json_file_path, 'w') as json_file:
         json.dump(json_data, json_file, indent=2)
 
+# Load the form schema template
+form_schema_path = 'O3_form_schema_template.json'
+with open(form_schema_path, 'r') as file:
+    form_schema = json.load(file)
+
 # Example usage
-xls_file_path = 'LIME EMR - Iraq Metadata - Release 1.xlsx'
+xls_file_path = 'F01-MHPSS_Baseline.xlsx'
 json_file_path = 'F01-MHPSS_Baseline.json'
-xls_to_json(xls_file_path, json_file_path)
+xls_to_json(xls_file_path, json_file_path, form_schema)
